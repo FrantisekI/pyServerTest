6361254c241cd4831c6e0a24bae8aa0040fea06c
FrantisekI <148909995+FrantisekI@users.noreply.github.com>
2023-11-22 20:53:26 +0100

    Add files via upload

--- another.py
+++ another.py
@@ -1,9 +1,30 @@
 from flask import Flask
+#import googleapiclient.discovery
+#api_key = 'AIzaSyDbVmYfDx_GALgXTRgbAydqPMun4FKQaF4'
+
 
 app = Flask(__name__)
 
 @app.route("/")
+
+
+
+
 def index():
-    return "<h1>Hello world</h1>"
+
+    '''api_service_name = "youtube"
+    api_version = "v3"
+
+    youtube = googleapiclient.discovery.build(
+    api_service_name, api_version, developerKey=api_key)
+    request = youtube.channels().list(
+        part="snippet,statistics",
+        id="UChFur_NwVSbUozOcF_F2kMg"
+    )
+    response = request.execute()'''
+    
+    #print(response)
+    return '<button type="button">Click Me!</button>'
+
 if __name__ == '__main__':
-   app.run(host='0.0.0.0', port=80)
+   app.run(host='127.0.0.1', port=80)
