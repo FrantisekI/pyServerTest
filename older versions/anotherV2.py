2919580a178a5d1c217af2729ee4ebfb62b45c97
FrantisekI <148909995+FrantisekI@users.noreply.github.com>
2024-02-24 22:34:09 +0100

    Update anotherV2.py

---
+++ older versions/anotherV2.py
@@ -0,0 +1,31 @@
+from flask import Flask
+#import googleapiclient.discovery
+#api_key = 'AIzaSyDbVmYfDx_GALgXTRgbAydqPMun4FKQaF4'
+
+
+app = Flask(__name__)
+
+@app.route("/")
+
+
+
+
+def index():
+
+    '''api_service_name = "youtube"
+    api_version = "v3"
+
+    youtube = googleapiclient.discovery.build(
+    api_service_name, api_version, developerKey=api_key)
+    request = youtube.channels().list(
+        part="snippet,statistics",
+        id="UChFur_NwVSbUozOcF_F2kMg"
+    )
+    response = request.execute()'''
+    print('text')
+    
+    #print(response)
+    return '<button type="button">Click Me!</button>'
+
+if __name__ == '__main__':
+   app.run(host='0.0.0.0', port=80)
